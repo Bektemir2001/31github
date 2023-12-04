2ba563c0016f2e0a308e5b3cc25581c1f5b03422
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-04 14:12:58 +0600

    add process

--- Scheduler/index.js
+++ Scheduler/index.js
@@ -11,5 +11,21 @@ class Process{
 function createProcessButton(event)
 {
     event.preventDefault();
-    console.log('create process')
+    console.log('gfjngdlkg')
+    document.getElementById('modalButtonID').click();
+}
+
+function createProcess(event)
+{
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let execution_time = document.getElementById('execution_time').value;
+    let arrival_time = document.getElementById('arrival_time').value;
+    document.getElementById('closeModalId').click();
+    processes.push(new Process(name, execution_time, arrival_time))
+}
+
+function displayList()
+{
+    
 }
